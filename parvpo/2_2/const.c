144a714893f70901b238cf90d779a1e6e69e3885
ThePigeonKing <thepigeonking@mail.ru>
2023-06-19 02:05:33 +0300

    2_2 done

---
+++ parvpo/2_2/const.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdio.h>
+int function(int b){
+    return b+1;
+}
+int main(){
+    clock_t tic = clock();
+    int a = 0;
+    double b;
+    double l = 1e10;
+    for(b=0; b < l; b++)
+      { a = function(b); };
+    clock_t toc = clock();
+    printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    return 0;
+}
+
